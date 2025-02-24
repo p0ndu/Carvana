8bcf02d3d85a9d21d47c21af68fd1b0365858bb1
p0ndu <161248564+p0ndu@users.noreply.github.com>
2025-02-24 16:04:40 +0000

    Importing tree from file
    Importing and building tree dynamically from file, allows one file for entire data structure. Must be inside src dir

--- src/TreeManager.cs
+++ src/TreeManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Reflection.Metadata.Ecma335;
 using System.Runtime.CompilerServices;
 using System.Linq; // for select and aggregate used in one-liner later on, not needed but let me have a little fun 
@@ -6,6 +7,24 @@ namespace Carvana;
 
 public static class TreeManager // static class which will manage the tree, will handle pruning and autocompletion
 {
+
+    public static Node BuildFromFile(string filePath)
+    {
+        Node root = new Node(""); // creates root node
+
+        string[] lines = File.ReadAllLines(filePath);
+        foreach (string line in lines)
+        {
+            string trimmedLine = line.Trim();
+            if (!string.IsNullOrEmpty(trimmedLine)) // if line is not null
+            {
+                InsertWord(root, trimmedLine); // inserts the word into the trie
+            }
+        }
+        return root; // returns root node as sort of header for whole tree
+    }
+    
+    
     public static void Prune(Node root) // wrapper function to call recursive pruning algorithm
     {
         bool outcome = PruneInternal(root);
@@ -74,5 +93,38 @@ public static class TreeManager // static class which will manage the tree, will
              root.GetParent().AddChild(child); // transfers parent relation to node above 'root', as 'root' is being removed
              root.GetParent().RemoveChild(root); // removes last pointer to 'root'
         }
+    }
+
+    private static void InsertWord(Node root, string word) // inserts word into the trie one char at a time
+    {
+        bool isNewBranch = false; // flag to mark wether we are inserting to a new branch, to avoid checking the children of nodes we just created
+        Node currentNode = root;
+        
+        foreach (char letter in word) // try to find each letter and if not found, add it. 
+        {
+            bool found = false; // flag tracking if a letter was found or not
+
+            if (!isNewBranch) // only checks children if it is not a new branch
+            {
+                foreach (Node child in currentNode.GetChildren()) // check eah child
+                {
+                    if (child.GetData().ToLower().Equals(letter.ToString(), StringComparison.OrdinalIgnoreCase)) // if node matches data being inserted
+                    {
+                        found = true;
+                        currentNode = child; // move to child node and search for the next letter
+                        break;
+                    }
+                }
+            }
+
+            if (!found) // if no matching node is found then make one
+            {
+                isNewBranch = true; // updates flag
+                Node newNode = new Node(letter.ToString(), currentNode); // create new node containing letter as data with current node as parent
+                currentNode.AddChild(newNode); // set new node to child of current node
+                currentNode = newNode; // continue searching for rest of word from newNode onwards, it wont be found as its a new branch meaning it will just create the word branching each time
+            }
+        }
+       Console.WriteLine("word - " + word + " inserted into trie \n"); 
     }
 }
