befa0b5908ba143d1a19ecfd050ea8d8c42a2ccc
SyntaxWizzard <ardagorkem1@gmail.com>
2025-02-26 15:37:35 +0000

    Created Search Algorithms; Binary,Ternary,Hash and modified Program class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,97 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carvana
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            // Get Personal Info from User
+            Console.Write("Enter Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter Age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Email: ");
+            string email = Console.ReadLine();
+
+            Console.Write("Enter Phone Number: ");
+            string phone = Console.ReadLine();
+
+            Console.Write("Enter Payment Info: ");
+            string paymentInfo = Console.ReadLine();
+
+            // Create Personal Info
+            PersonalInfo person = new PersonalInfo(name, age, email, phone, paymentInfo);
+
+            // Get License ID
+            Console.Write("Enter License ID: ");
+            string licenseId = Console.ReadLine();
+
+            // Create Customer with License ID and Personal Info
+            Customer customer2 = new Customer(licenseId, person);
+
+            // Add History
+            Console.Write("Enter a damage history record: ");
+            string damage = Console.ReadLine();
+            customer2.getHistory().addDamage(damage);
+
+            Console.Write("Enter a rental history record: ");
+            string rental = Console.ReadLine();
+            customer2.getHistory().addRental(rental);
+
+            // Display Customer Info
+            Console.WriteLine("\nCustomer Info:");
+            Console.WriteLine(customer2);
+
+            ///////////////////////SEARCH ALGOS/////////////////////////////
+
+            // Binary & Ternary Search 
+            var numbers = new List<int> { 90, 155, 168, 180, 197, 201 };
+
+            Console.Write("\nEnter a number to search (Binary Search): ");
+            int searchBinary = int.Parse(Console.ReadLine());
+            int binaryIndex = SearchAlgorithms<int>.BinarySearch(numbers, searchBinary);
+            Console.WriteLine($"Binary Search: Index of {searchBinary} is {binaryIndex}");
+
+            Console.Write("\nEnter a number to search (Ternary Search): ");
+            int searchTernary = int.Parse(Console.ReadLine());
+            int ternaryIndex = SearchAlgorithms<int>.TernarySearch(numbers, searchTernary);
+            Console.WriteLine($"Ternary Search: Index of {searchTernary} is {ternaryIndex}");
+
+            // Hash Search 
+            var carDict = new Dictionary<string, string>
+        {
+            { "AA20AA", "BMW" },
+            { "MD20XYZ", "Toyota" },
+            { "MD10XYZ", "Audi" },
+            { "MD11XYZ", "Seat" }
+        };
+
+            Console.Write("\nEnter Car License Plate to Search: ");
+            string searchPlate = Console.ReadLine();
+
+            if (SearchAlgorithms<string>.HashSearch(carDict, searchPlate, out string car))
+                Console.WriteLine($"Found car: {car}");
+            else
+                Console.WriteLine("Car not found");
+        }
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
---
+++ SearchAlgo.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class SearchAlgorithms<T> where T : IComparable<T>
+{
+    // Binary
+    public static int BinarySearch(List<T> list, T key)
+    {
+        // List is sorted
+        list.Sort(); 
+        return BinarySearchRecursive(list, key, 0, list.Count - 1);
+    }
+
+    private static int BinarySearchRecursive(List<T> list, T key, int left, int right)
+    {
+        if (left > right)
+            return -1; 
+
+        int mid = left + (right - left) / 2;
+        int comparison = list[mid].CompareTo(key);
+
+        if (comparison == 0)
+            return mid;
+        else if (comparison > 0)
+            return BinarySearchRecursive(list, key, left, mid - 1);
+        else
+            return BinarySearchRecursive(list, key, mid + 1, right);
+    }
+
+    // Ternary Search
+    public static int TernarySearch(List<T> list, T key)
+    {
+        list.Sort();
+        return TernarySearchRecursive(list, key, 0, list.Count - 1);
+    }
+
+    private static int TernarySearchRecursive(List<T> list, T key, int left, int right)
+    {
+        if (left > right)
+            return -1;
+
+        int mid1 = left + (right - left) / 3;
+        int mid2 = right - (right - left) / 3;
+
+        if (list[mid1].CompareTo(key) == 0)
+            return mid1;
+        if (list[mid2].CompareTo(key) == 0)
+            return mid2;
+
+        if (key.CompareTo(list[mid1]) < 0)
+            return TernarySearchRecursive(list, key, left, mid1 - 1);
+        else if (key.CompareTo(list[mid2]) > 0)
+            return TernarySearchRecursive(list, key, mid2 + 1, right);
+        else
+            return TernarySearchRecursive(list, key, mid1 + 1, mid2 - 1);
+    }
+
+    // Hash Search
+    public static bool HashSearch<TKey, TValue>(Dictionary<TKey, TValue> dict, TKey key, out TValue value)
+    {
+        return dict.TryGetValue(key, out value);
+    }
+}
+
